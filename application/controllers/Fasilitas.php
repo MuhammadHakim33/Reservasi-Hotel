5b384e83eb88c1981a96a1ceda74d58137f217eb
Muhammad Hakim <phakimb33@gmail.com>
2022-03-25 09:31:16 +0700

    Revisi Halaman Fasilitas

--- application/controllers/Fasilitas.php
+++ application/controllers/Fasilitas.php
@@ -6,9 +6,8 @@ class Fasilitas extends CI_Controller {
 	public function index()
 	{
 		$data["title"] = "Fasilitas";
-        $this->load->view('staff/view_header', $data);
-        $this->load->view('staff/view_navside');
-		$this->load->view('staff/view_Fasilitas');
-        $this->load->view('staff/view_footer');
+        $this->load->view('user/view_header', $data);
+		$this->load->view('user/view_Fasilitas');
+        $this->load->view('user/view_footer');
 	}
 }
---
+++ application/views/user/view_fasilitas.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <!-- List Fasilitas -->
+    <div class="row g-3">
+        <!-- Divider -->
+        <div class="col-12 mt-1"><hr></div>
+        <!-- Fasilitas -->
+        <div class="col col-6">
+            <div class="card mb-3">
+                <img src="<?php base_url();?> assets/img/room1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- Nama Fasilitas -->
+                    <h5 class="card-text text-muted"><small>Lapangan Bola</small></h5>
+                </div>
+            </div>
+        </div>
+        <!-- Fasilitas -->
+        <div class="col col-6">
+            <div class="card mb-3">
+                <img src="<?php base_url();?> assets/img/room1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <!-- Nama Fasilitas -->
+                    <h5 class="card-text text-muted"><small>Lapangan Bola</small></h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
