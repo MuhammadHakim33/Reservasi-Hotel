e62a07aba58c112b06aacb11a2c969ddb4a85fb3
Muhammad Hakim <phakimb33@gmail.com>
2022-03-30 18:24:22 +0700

    Sistem Cek Ketersediaan Kamar

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -2,12 +2,53 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Beranda extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Model_main');
+	}
 
 	public function index()
 	{
 		$data["title"] = "Beranda";
+		$data["kamar"] = $this->Model_main->get_data('tbl_kamar');
+		$data["reservasi"] = null;
+
+		$check_in = $this->input->post('check_in');
+		$check_out = $this->input->post('check_out');
+		$tipe_kamar = $this->input->post('tipe_kamar');
+		$jumlah_kamar = $this->input->post('jumlah_kamar');
+
+		if(!empty($check_in) && !empty($check_out)) {
+			$result = $this->cek_ketersediaan_kamar($check_in, $tipe_kamar, $jumlah_kamar);
+			$data["reservasi"] = ($result == true ) ? 'tersedia' : 'penuh';
+		}
+
 		$this->load->view('user/view_header', $data);
-		$this->load->view('user/view_beranda');
+		$this->load->view('user/view_beranda', $data);
 		$this->load->view('user/view_footer');
+	}
+
+
+	public function cek_ketersediaan_kamar($check_in, $tipe_kamar, $jumlah_kamar) {
+		$table_one = 'tbl_pemesanan_kamar';
+		$table_two = 'tbl_kamar';
+		$where_one = ['check_in_kamar' => $check_in, 'id_kamar' => $tipe_kamar];
+		$where_two = ['id' => $tipe_kamar];
+
+		$tbl_pemesanan_kamar = $this->Model_main->get_data($table_one, $where_one);
+		$tbl_kamar = $this->Model_main->get_data($table_two, $where_two);
+
+		$kamar_terisi = 0;
+
+		foreach($tbl_pemesanan_kamar as $kamar) {
+			$kamar_terisi += $kamar['jumlah_kamar'];
+		}
+
+		if(($kamar_terisi + $jumlah_kamar) <= $tbl_kamar[0]['jumlah_kamar']) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- application/views/user/view_beranda.php
+++ application/views/user/view_beranda.php
@@ -16,67 +16,73 @@
             </div>
         </div>
         <!-- Cek Kamar -->
-        <div class="col-12 my-4 offset-md-1 col-md-10">
-            <div class="card shadow-lg border-0">
+        <div class="col-12 my-4 ">
+            <div class="card shadow-lg border-0 ">
                 <div class="card-header text-primary">Cek Kamar</div>
-                <form class="card-body row gy-3" action="" method="">
-                    <div class="col-12 col-sm col-lg-4 mb-lg-0">
-                        <label for="check_in" class="form-label">Check In</label>
-                        <input type="date" min="<?= date('Y-m-d'); ?>" class="form-control" id="check_in" name="check_in">
+                <form class="card-body row gy-3" action="" method="POST">
+                    <div class="col-12 col-md col-lg-3 mb-lg-0">
+                        <label for="check_in" class="form-label text-muted">Check In</label>
+                        <input type="date" min="<?= date('Y-m-d'); ?>" class="form-control" id="check_in" name="check_in" required>
                     </div>
-                    <div class="col-12 col-sm col-lg-4 mb-lg-0">
-                        <label for="check_out" class="form-label">Check Out</label>
-                        <input type="date" min="<?= date('Y-m-d'); ?>" class="form-control" id="check_out" name="check_out">
+                    <div class="col-12 col-md col-lg-3 mb-lg-0">
+                        <label for="check_out" class="form-label text-muted">Check Out</label>
+                        <input type="date" min="<?= date('Y-m-d'); ?>" class="form-control" id="check_out" name="check_out" required>
                     </div>
-                    <div class="col col-lg-2 mb-lg-0">
-                        <label for="jumah_kamar" class="form-label">Jumlah Kamar</label>
-                        <input type="number" min="1" class="form-control" id="jumah_kamar" name="jumah_kamar" value="1">
+                    <div class="col-12 col-md-6 col-lg-2 mb-lg-0">
+                        <label class="form-label text-muted">Tipe Kamar</label>
+                        <select class="form-select" aria-label="Default select example" name="tipe_kamar">
+                            <?php foreach($kamar as $data) :?>
+                                <option value="<?= $data['id'] ?>"><?= $data['nama_tipe_kamar'] ?></option>
+                            <?php endforeach;?>
+                        </select>
+                    </div>
+                    <div class="col col-md-6 col-lg-2 mb-lg-0">
+                        <label for="jumah_kamar" class="form-label text-muted">Jumlah Kamar</label>
+                        <input type="number" min="1" class="form-control" id="jumah_kamar" name="jumlah_kamar" value="1" required>
                     </div>
                     <div class="col col-lg-2 mb-lg-0">
                         <label class="form-label opacity-0">Button</label>
                         <br>
-                        <button type="submit" class="btn btn-primary">Cek</button>
+                        <button type="submit" class="btn btn-primary">Periksa</button>
                     </div>
                 </form>
             </div>
         </div>
+
         <!-- Form Reservasi -->
-        <div class="col-12 my-4 offset-md-1 col-md-10">
-            <!-- Alert -->
-            <!-- <div class="alert alert-primary text-center" role="alert">Kamar Kosong Tersedia - Pesan Sekarang !</div> -->
-            <!-- Form -->
-            <!-- <div class="card">
-                <div class="card-header text-primary">Form Reservasi</div>
-                <form class="card-body row gy-4" action="" method="">
-                    <div class="col-12">
-                        <label for="nama_pemesan" class="form-label text-muted">Nama Pemesan</label>
-                        <input type="text" class="form-control" id="nama_pemesan" name="nama_pemesan">
-                    </div>
-                    <div class="col-12">
-                        <label for="nama_tamu" class="form-label text-muted">Nama Tamu</label>
-                        <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
-                    </div>
-                    <div class="col-12 col-sm-6 col-lg-5">
-                        <label for="email" class="form-label text-muted">Email</label>
-                        <input type="text" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="col-12 col-sm-6 col-lg-4">
-                        <label for="telp" class="form-label text-muted">No. Handphone</label>
-                        <input type="text" class="form-control" id="telp" name="telp">
-                    </div>
-                    <div class="col-12 col-lg-3">
-                        <label class="form-label text-muted">Tipe Kamar</label>
-                        <select class="form-select" aria-label="Default select example">
-                            <option value="1">Standar</option>
-                            <option value="2">Deluxe</option>
-                            <option value="3">Superior</option>
-                        </select>
-                    </div>
-                    <div>
-                        <button type="submit" class="btn btn-primary">Pesan</button>
-                    </div>
-                </form>
-            </div>
-        </div> -->
+        <div class="col-12 my-4 offset-md-2 col-md-8">
+            <?php if($reservasi != null && $reservasi == 'tersedia') : ?>
+                <!-- Alert -->
+                <div class="alert alert-primary text-center" role="alert">Kamar Kosong Tersedia - Pesan Sekarang !</div>
+                <!-- Form -->
+                <div class="card">
+                    <div class="card-header text-primary">Form Reservasi</div>
+                    <form class="card-body row gy-4" action="" method="">
+                        <div class="col-12">
+                            <label for="nama_pemesan" class="form-label text-muted">Nama Pemesan</label>
+                            <input type="text" class="form-control" id="nama_pemesan" name="nama_pemesan">
+                        </div>
+                        <div class="col-12">
+                            <label for="nama_tamu" class="form-label text-muted">Nama Tamu</label>
+                            <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <label for="email" class="form-label text-muted">Email</label>
+                            <input type="text" class="form-control" id="email" name="email">
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <label for="telp" class="form-label text-muted">No. Handphone</label>
+                            <input type="text" class="form-control" id="telp" name="telp">
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">Pesan</button>
+                        </div>
+                    </form>
+                </div>
+            <?php endif; ?>
+            <?php if($reservasi != null && $reservasi == 'penuh') : ?>
+                <div class="alert alert-danger text-center" role="alert">Kamar Penuh !</div>            
+            <?php endif; ?>
+        </div>
     </div>
 </div>
